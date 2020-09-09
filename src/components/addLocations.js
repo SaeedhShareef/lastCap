fbdb12ffe0174795266d6918a0f27d76de46062a
Saeed Shareef <saeedhshareef@gmail.com>
2020-09-09 16:49:18 -0400

    d

--- src/components/addLocations.js
+++ src/components/addLocations.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // import e from 'express';
 import ToggleButton from 'react-bootstrap/ToggleButton'
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
-import Button from 'react-bootstrap/button'
+import {Button} from 'react-strap'
 export default class addLocations extends Component {
             constructor(props) {
                 super(props);
