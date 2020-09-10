a8c3e7427845f24b46895c00eeee684e58eefe7b
Saeed Shareef <saeedhshareef@gmail.com>
2020-09-10 13:45:24 -0400

    push

---
+++ client/build/precache-manifest.2540b719157926700954809968cd9315.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0155f79024d9610d067078c226c20ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fec3e85f16ad2236d34e",
+    "url": "/static/css/2.3e0c83f9.chunk.css"
+  },
+  {
+    "revision": "af4fd03f2f754e01f42d",
+    "url": "/static/css/main.8c4bb4b8.chunk.css"
+  },
+  {
+    "revision": "fec3e85f16ad2236d34e",
+    "url": "/static/js/2.e237ef45.chunk.js"
+  },
+  {
+    "revision": "db4833b02f62bd470bb1579a2ec8a284",
+    "url": "/static/js/2.e237ef45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4fd03f2f754e01f42d",
+    "url": "/static/js/main.fd301ed1.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  },
+  {
+    "revision": "fabbc2e697c207f4ccfe7dd39ccfaab5",
+    "url": "/static/media/CAC3.fabbc2e6.jpg"
+  },
+  {
+    "revision": "822e893d382499688b010bc344e1b3d0",
+    "url": "/static/media/CAC4.822e893d.jpg"
+  },
+  {
+    "revision": "3835f6610873d530ccdfc84f209b3e02",
+    "url": "/static/media/CAM.3835f661.jpg"
+  },
+  {
+    "revision": "db27b53a5ea42d47ed24e9a71f15a491",
+    "url": "/static/media/Krohn.db27b53a.jpg"
+  },
+  {
+    "revision": "5c6fa01a38477015f515f6032d3f8f66",
+    "url": "/static/media/Tourist1.5c6fa01a.jpg"
+  },
+  {
+    "revision": "2db01b62dc8134ab6ecda40bb19d458d",
+    "url": "/static/media/Union2.2db01b62.jpg"
+  },
+  {
+    "revision": "e4a602014d53c46b5cccd40aa96d8383",
+    "url": "/static/media/Union4.e4a60201.jpg"
+  },
+  {
+    "revision": "8b97b5743732f4a627610192629914aa",
+    "url": "/static/media/arcade.8b97b574.jpg"
+  },
+  {
+    "revision": "ce99fac92e2f6954a8bf8875cbf9f71e",
+    "url": "/static/media/arcade2.ce99fac9.jpg"
+  },
+  {
+    "revision": "430e4a316b8d3c6ccfe3c9bcb966b986",
+    "url": "/static/media/artPage11.430e4a31.jpg"
+  },
+  {
+    "revision": "48a04ec81ca7875061985d6dd27eb617",
+    "url": "/static/media/artPageLarge.48a04ec8.jpg"
+  },
+  {
+    "revision": "19479013b03071e7626b0ebf2a3c7da4",
+    "url": "/static/media/artPageLarge2.19479013.jpg"
+  },
+  {
+    "revision": "debaf555361f81fde2b9f308f4545fc6",
+    "url": "/static/media/artPageLarge3.debaf555.jpg"
+  },
+  {
+    "revision": "4b4cde05e1f2341cc4d7d1689192c985",
+    "url": "/static/media/artPageLarge4.4b4cde05.jpg"
+  },
+  {
+    "revision": "714044d317be0ba7d69859bc9d243c49",
+    "url": "/static/media/artPageLarge5.714044d3.jpg"
+  },
+  {
+    "revision": "245d5956f3f1d0feb70c2d88bb75a3f0",
+    "url": "/static/media/image.245d5956.jpeg"
+  },
+  {
+    "revision": "dbb13dd35ae6a24605fdbee3dbe99290",
+    "url": "/static/media/learn.dbb13dd3.jpg"
+  },
+  {
+    "revision": "770d9ee3bb00eacca5f5a2e170f29e5c",
+    "url": "/static/media/newport1.770d9ee3.jpg"
+  },
+  {
+    "revision": "a9c5dbff514079accafd8e615c4382a8",
+    "url": "/static/media/ohiomap.a9c5dbff.png"
+  },
+  {
+    "revision": "b5cd8a9e22991eb58859e0ab520766ec",
+    "url": "/static/media/otherMuseum.b5cd8a9e.jpg"
+  },
+  {
+    "revision": "a909444b0a3639354e822ada2448005d",
+    "url": "/static/media/planeicon.a909444b.png"
+  },
+  {
+    "revision": "0d2ae1db0de6b99862b655e62f891ca5",
+    "url": "/static/media/shopping.0d2ae1db.jpg"
+  },
+  {
+    "revision": "a615fb088958217c220334b3e6d4ada1",
+    "url": "/static/media/skatepark3.a615fb08.jpg"
+  },
+  {
+    "revision": "87cf78eac6d964e719e25e5ef33f2b0a",
+    "url": "/static/media/skater7.87cf78ea.jpg"
+  },
+  {
+    "revision": "b9742001c2410413a4be6f530a7dec17",
+    "url": "/static/media/skater9.b9742001.jpg"
+  },
+  {
+    "revision": "c1682e90f7a4ea1aaef9fb44722ac8d7",
+    "url": "/static/media/sky.c1682e90.jpg"
+  },
+  {
+    "revision": "e608ed9f80689ef0476b5981c792892d",
+    "url": "/static/media/skybottom.e608ed9f.png"
+  },
+  {
+    "revision": "c33ed44b08768d120441280160a169e1",
+    "url": "/static/media/skyline.c33ed44b.jpg"
+  },
+  {
+    "revision": "f9cb70431aea9e0100aed2a054a51139",
+    "url": "/static/media/tourist food.f9cb7043.jpg"
+  },
+  {
+    "revision": "4ba598238ed78ac41c3b7901d43e075a",
+    "url": "/static/media/tourist2.4ba59823.jpg"
+  },
+  {
+    "revision": "6ba42910513294c5f2e39a49eba4c118",
+    "url": "/static/media/tourist3.6ba42910.jpg"
+  },
+  {
+    "revision": "285f2acc390218df46cd4fb9fea34ec1",
+    "url": "/static/media/tourists4.285f2acc.jpg"
+  },
+  {
+    "revision": "8f2d043b58cd642d3e47d37292d3e8df",
+    "url": "/static/media/tourists6.8f2d043b.jpg"
+  },
+  {
+    "revision": "ce9ed542a06121c03aa42098bc2c4ab0",
+    "url": "/static/media/view.ce9ed542.jpg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/capstone/precache-manifest.82984f1ce97e4369b71bc52d9fe51ff7.js"
+  "/precache-manifest.2540b719157926700954809968cd9315.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/capstone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
